d6c9b51a18c5d096607236cf367262387dd7323c
Christian Müller <mail@kb135.de>
2014-06-05 19:02:20 +0200

    fixed typo

--- sample.py
+++ sample.py
@@ -5,28 +5,13 @@ from httphandler import HTTPHandler
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
+simple_handler = HTTPHandler('localhost', '/api/logger')
+https_handler = HTTPHandler('localhost:443', '/api/logger', method='POST', secure=True)
+auth_handler = HTTPHandler('localhost:443', '/api/logger', method='POST', secure=True,
+                           authorization=('username', 'password'))
 
-simple_handler = HTTPHandler(
-    'localhost',
-    '/api/logger'
-)
-simple_handler.debug('some debug message')
+logger.addHandler(simple_handler)
+logger.addHandler(https_handler)
+logger.addHandler(auth_handler)
 
-https_handler = HTTPHandler(
-    'localhost:443',
-    '/api/logger',
-    secure=True
-)
-https_handler.debug('some debug message over https')
-
-auth_handler = HTTPHandler(
-    'localhost:443',
-    '/api/logger',
-    method='POST',
-    secure=True,
-    authorization=('username', 'password')
-)
-auth_handler.debug('super secure debug message posted over https')
-
-
-
+logger.debug('some debug message')
