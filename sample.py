191c803c596b27b99150ba0568ef1d4f922b6a94
Christian Müller <mail@kb135.de>
2014-06-05 18:53:15 +0200

    added sample.py

---
+++ sample.py
@@ -0,0 +1,32 @@
+import logging
+from httphandler import HTTPHandler
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+simple_handler = HTTPHandler(
+    'localhost',
+    '/api/logger'
+)
+simple_handler.debug('some debug message')
+
+https_handler = HTTPHandler(
+    'locahost:443',
+    '/api/logger',
+    secure=True
+)
+https_handler.debug('some debug message over https')
+
+auth_handler = HTTPHandler(
+    'localhost:443',
+    '/api/logger',
+    method='POST',
+    secure=True,
+    authorization=('username', 'password')
+)
+auth_handler.debug('super secure debug message posted over https')
+
+
+
